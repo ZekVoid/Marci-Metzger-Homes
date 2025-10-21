2d4006cecaa186ae2999d77fa75d9eca8c753b7e
ZekVoid <carlojamir91401@gmail.com>
2025-10-21 13:12:01 +0800

    commit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-window.addEventListener('scroll', function() {
-    const nav = document.getElementById('mainNav');
-
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('mainNav');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 });
